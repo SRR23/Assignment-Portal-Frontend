deb36e153406acef553f2d3110efaf8cb6a8a243
SRR23 <srrimon267@gmail.com>
2025-08-02 19:48:09 +0600

    added

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3001",
-    API_URL: process.env.API_URL || "http://localhost:3000", // Fallback to backend URL
+    API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000", // Fallback to backend URL
   },
 };
 
